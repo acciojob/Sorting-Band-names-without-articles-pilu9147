bb0e5ef0fabcdd1fa0a92ff82ce793b6b50b84ac
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 18:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+//your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+const ul = document.querySelector('ul');
+ 
+const removeAticles = () => {
+    const res=touristSpots.map(element => {
+        let indivialEle = element.split(' ');
+        const tempRes = indivialEle.filter(item => {
+            
+            if (item == 'A' || item =='The' || item == 'An') {
+                return;
+            } else {
+                return item;
+            }
+        })
+        return tempRes.join(' ');
+    })
+    res.sort();
+    for (let i = 0; i < res.length; i++){
+        const li = document.createElement('li');
+        li.innerText = res[i];
+        ul.append(li);
+    }
+}
+removeAticles();
